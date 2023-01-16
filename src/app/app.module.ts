7883ee920d5c58a6f7ed4af1da0ab7fe7d149d78
harnalerohan <harnalerohan@gmail.com>
2023-01-16 12:25:25 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HeroSectionComponent } from './shared/components/hero-section/hero-section.component';
+import { UpcomingFairs } from './shared/components/upcomingFairs/upcomingFairs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    UpcomingFairs,
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/upcomingFairs.interface.ts
@@ -0,0 +1,29 @@
+export interface IUpcomingFairs {
+  fairId: string,
+  fairName: string,
+  startDate: number,
+  endDate: number,
+  timezoneId: string,
+  location: {
+    addressOne: string ,
+    addressTwo: string,
+    city: string,
+    state: string,
+    country: string,
+    zip: string,
+  },
+  numberOfUsersRegistered: number,
+  numberOfSchoolsRegistered: number,
+  isCandidateRegistered: boolean,
+  bannerUrl: string
+  showRegisteredUsersToCandidate: boolean,
+  showRegisteredUsersToSchools: boolean,
+  showRegisteredSchoolsToCandidate: boolean,
+  showRegisteredSchoolsToSchools: boolean,
+  fairStatus: string,
+  fairStartTime: string,
+  fairEndTime: string,
+  type: string,
+  candidateDescription: string
+  schoolDescription: string
+}
